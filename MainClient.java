b7019db717cdc07fec3aa8cc472462ddf94d4271
Emanuelzk1533 <acaymanuel16@gmail.com>
2025-08-31 14:56:28 -0300

    MainClient.java

---
+++ MainClient.java
@@ -0,0 +1,8 @@
+public class MainClient {
+    public static void main(String[] args) {
+        System.out.println("Iniciando Xurras Client 1.8.9...");
+
+        ModuleManager.initModules();
+        HudEditorScreen.open();
+    }
+}
